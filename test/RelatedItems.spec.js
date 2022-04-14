08456b22aa2f93e47e7829064411f8559aa6840e
pschaefferkoetter <prschaefferkoetter@gmail.com>
2022-04-13 18:25:38 -0600

    updates to test

--- test/RelatedItems.spec.js
+++ test/RelatedItems.spec.js
@@ -8,7 +8,7 @@
  import RelatedItems from '../src/components/RelatedItems/index.js';
  import data from './data.js'
  import ReactDOM from "react-dom";
- import {render, fireEvent, waitFor, screen} from '@testing-library/react';
+ //import {render, fireEvent, waitFor, screen} from '@testing-library/react';
 
  it("renders without crashing", () => {
   const div = document.createElement("div");
